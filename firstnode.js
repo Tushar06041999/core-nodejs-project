c1d5d24a922599e9a484cd2b2747c516b0a0d728
Tushar06041999 <tushar06041999@gmail.com>
2022-04-21 22:00:11 +0600

    op and path

--- firstnode.js
+++ firstnode.js
@@ -1,6 +1,32 @@
-var http = require('http');
+// var http = require('http');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-}).listen(8080);
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   res.end('Hello World!');
+// }).listen(8080);
+
+
+//path Declearation
+
+
+const path =require("path");
+const pathName="C:/web <devlopment /nodejs folder/first node js/firstnode.js" ;
+
+// console.log(path.basename(pathName));  //path Base Name Declicartion
+// console.log(path.dirname(pathName));   //Dire4ctory Name
+// console.log(path.extname(pathName));   //Extension Name
+// console.log(path.parse(pathName));    //It's show object
+// console.log(path.delimiter);
+// console.log(path.resolve(pathName));
+
+//os method Explore
+
+const os=require("os");
+console.log(os.platform());
+console.log(os.version());
+console.log(os.hostname());
+console.log(os.loadavg());
+console.log(os.homedir());
+console.log(os.release());
+console.log(os.freemem());
+console.log(os.cpus());
